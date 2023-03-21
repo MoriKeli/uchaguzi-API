53157e6c6c1f2b4b593bb59e4be4cbe348af9c6c
brocode <kelimbth@gmail.com>
2023-03-21 12:54:19 +0300

    added a view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import AspirantsSerializer
 
-# Create your views here.
+class VieForElectoralPostView(APIView):
+    def post(self, request):
+        serializer = AspirantsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
