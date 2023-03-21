b651a8c175a7fb15be17ce2b506dc0b6e6f50b39
brocode <kelimbth@gmail.com>
2023-03-21 10:16:35 +0300

    added login and voter registration URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
+    path('login/', views.LoginView.as_view(), name='user_login'),
     path('signup/', views.SignupView.as_view(), name='signup'),
+    path('voter-registration/', views.VoterRegistrationView.as_view(), name='voter_registration'),
+
 ]
