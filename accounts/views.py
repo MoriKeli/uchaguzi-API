016226cf6c3e088bd0c037be393a0f727485b1ba
brocode <kelimbth@gmail.com>
2023-03-21 10:16:09 +0300

    added LoginView and VoterRegistrationView

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,38 @@
+from django.contrib import auth
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializers import SignupSerializer
+from rest_framework.exceptions import AuthenticationFailed
+from .serializers import SignupSerializer, VoterRegistrationSerializer
+from datetime import datetime, timedelta
+import jwt
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = auth.authenticate(email=email, password=password)
+        
+        if user is None:
+            raise AuthenticationFailed('INVALID CREDENTIALS!!!')
+
+        payload = {
+            "id": user.id,
+            "exp": datetime.utcnow() + timedelta(minutes=60),    # token expires after 1 hour.
+            "iat": datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+        response.data = {"User logged in ..."}
+
+        # store token as a cookies
+        response.set_cookie(key='jwt', value=token, httponly=True)
+
+        return response
 
 
 class SignupView(APIView):
@@ -12,4 +43,11 @@ class SignupView(APIView):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class VoterRegistrationView(APIView):
+    def post(self, request):
+        serializer = VoterRegistrationSerializer(data=request.data, instance=request.user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(is_voter=True)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
