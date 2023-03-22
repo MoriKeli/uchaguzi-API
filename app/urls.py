03de1481a0450a058ce7fa0b93a2bebd786c88b7
brocode <kelimbth@gmail.com>
2023-03-22 15:09:51 +0300

    added new URL paths

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('vie-for-post/', views.VieForElectoralPostView.as_view(), name='vie_for_electoral_seat'),
+    path('vote/', views.VotingView.as_view(), name='cast_vote'),
+    path('polls/', views.PollingView.as_view(), name='polls'),
+    path('nominations/', views.NominationView.as_view(), name='nominate_candidate'),
 
 ]
