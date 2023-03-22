7b38c8d3f9c5578ef1b5db3135509e67e90924bb
brocode <kelimbth@gmail.com>
2023-03-22 15:07:08 +0300

    created new serializers

--- app/serializers.py
+++ app/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
-from .models import Aspirants
+from .models import Aspirants, Nominations, VotingResults, PollsResults
 
 class AspirantsSerializer(serializers.ModelSerializer):
     """ Aspirants can use this serializer to fill in their details when vying for an electoral post. """
 
     class Meta:
         model = Aspirants
+        fields = '__all__'
+
+class NominateAspirantsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nominations
+        fields = '__all__'
+
+
+class VotingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VotingResults
+        fields = '__all__'
+
+class PollingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PollsResults
         fields = '__all__'
 
