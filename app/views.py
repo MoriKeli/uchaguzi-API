8d7ced5a0cf4045db8779792ab45b9d45706a924
brocode <kelimbth@gmail.com>
2023-03-23 12:28:57 +0300

    added permission_classes, authentication_classes and decorator

--- app/views.py
+++ app/views.py
@@ -1,17 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
+from django.contrib.auth.decorators import user_passes_test
 from .serializers import AspirantsSerializer, NominateAspirantsSerializer, VotingSerializer, PollingSerializer
 from .models import Aspirants, VotingResults, PollsResults
 
+
 class VieForElectoralPostView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    @user_passes_test(lambda user: user.is_voter is True)
     def post(self, request):
         serializer = AspirantsSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(name=request.user)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
 
 class VotingView(APIView):
     def object_ID(self, aspirantID):
@@ -20,7 +26,9 @@ class VotingView(APIView):
         except Aspirants.DoesNotExist:
             return Response({"Aspirant not found or was never nominated ..."}, status=status.HTTP_404_NOT_FOUND)
     
-
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    @user_passes_test(lambda user: user.is_voter is True)
     def post(self, request, aspirantID):
         serializer = VotingSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -38,6 +46,9 @@ class PollingView(APIView):
         except Aspirants.DoesNotExist:
             return Response({"Aspirant not found or was never nominated."}, status=status.HTTP_404_NOT_FOUND)
 
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    @user_passes_test(lambda user: user.is_voter is True)
     def post(self, request, aspirantID):
         serializer = PollingSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -52,7 +63,9 @@ class NominationView(APIView):
         except Aspirants.DoesNotExist:
             return Response({"Aspirant not found or was never nominated."}, status=status.HTTP_404_NOT_FOUND)
 
-    
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    @user_passes_test(lambda user: user.is_official is True)
     def post(self, request, aspirantID):
         serializer = NominateAspirantsSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
